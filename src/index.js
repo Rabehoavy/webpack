d8f37497b8b52f357a8eb07295956ffb21cad8df
Rabehoavy <rabehoavy@gmail.com>
2020-04-28 16:54:52 +0400

    Compiling from SCSS to CSS (mini-css-extract-plugin)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require("../assets/stylesheets/styles.scss");
 document.write("Je débute avec Webpack! ");
 let a = "J'apprends Webpack !";
 document.write(a);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
 
 let config = {
     entry: "./src/index.js",
@@ -12,8 +14,29 @@ let config = {
           test: /\.js$/,
           exclude: /node_modules/,
           loader: "babel-loader"
-        }]
-    }
+        },
+        {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+            {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                hmr: process.env.NODE_ENV === 'development',
+                },
+            },
+            'css-loader',
+            'postcss-loader',
+            'sass-loader',
+            ],
+          }]
+      },
+      plugins: [
+        new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // both options are optional
+          filename: devMode ? '[name].css' : '[name].[hash].css',
+          chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+        }),
+      ]
   }
-  
   module.exports = config;
